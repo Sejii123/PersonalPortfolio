3946e5d5a662df6258a807694accad2d86cd0e5e
Sejii Rizil <sarizil.student@asiancollege.edu.ph>
2026-08-07 21:51:32 +0800

    add css and js

---
+++ script.js
@@ -0,0 +1,42 @@
+const texts = [
+    "Web Developer",
+    "Programmer",
+    "Student"
+];
+
+const typingText = document.getElementById("typing-text");
+
+let textIndex = 0;
+let charIndex = 0;
+let deleting = false;
+
+function typeEffect() {
+    const currentText = texts[textIndex];
+
+    if (!deleting) {
+        typingText.textContent = currentText.substring(0, charIndex + 1);
+        charIndex++;
+
+        if (charIndex === currentText.length) {
+            deleting = true;
+            setTimeout(typeEffect, 1500);
+            return;
+        }
+    } else {
+        typingText.textContent = currentText.substring(0, charIndex - 1);
+        charIndex--;
+
+        if (charIndex === 0) {
+            deleting = false;
+            textIndex++;
+
+            if (textIndex === texts.length) {
+                textIndex = 0;
+            }
+        }
+    }
+
+    setTimeout(typeEffect, deleting ? 70 : 120);
+}
+
+typeEffect();
