24b0dc6ef48b9b4fbbeaf17376789e7d1d8a14d3
Sejii Rizil <sarizil.student@asiancollege.edu.ph>
2026-08-13 10:44:17 +0800

    fix the button

--- script.js
+++ script.js
@@ -50,4 +50,10 @@ themeToggle.addEventListener("click", function() {
 
     document.body.classList.toggle("light-mode");
 
+    if (document.body.classList.contains("light-mode")) {
+        themeToggle.textContent = "☀️";
+    } else {
+        themeToggle.textContent = "🌙";
+    }
+
 });
